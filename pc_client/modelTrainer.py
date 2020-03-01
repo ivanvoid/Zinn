d1abb2b142eb088724303771712b3d939a6cebf0
veax-void <caxaev95@gmail.com>
2020-03-01 11:11:35 +0800

    Added target render for user and ai

--- pc_client/modelTrainer.py
+++ pc_client/modelTrainer.py
@@ -7,7 +7,8 @@ img = np.zeros(win_shape)
 
 img = img + 255
 
-x_len = 5
+# User cross target
+x_len = 15
 x_center = win_shape[0] // 2
 x_coords = [
         (x_center - x_len, x_center - x_len), 
@@ -15,8 +16,20 @@ x_coords = [
         (x_center + x_len, x_center - x_len),
         (x_center - x_len, x_center + x_len)]
 
-img = cv2.line(img, x_coords[0], x_coords[1], (0,0,0), 2, lineType=cv2.LINE_AA)
-img = cv2.line(img, x_coords[2], x_coords[3], (0,0,0), 2, lineType=cv2.LINE_AA)
+# AI circle prediction
+ai_coords = (256,256)
+r = 10
+
+
+# Render objects
+img = cv2.circle(img, ai_coords, r, (0,255,0), -1)
+
+img = cv2.line(img, x_coords[0], x_coords[1], (0,0,0), 
+        2, lineType=cv2.LINE_AA)
+img = cv2.line(img, x_coords[2], x_coords[3], (0,0,0), 
+        2, lineType=cv2.LINE_AA)
+img = cv2.circle(img, (x_center,x_center), 5, (255,255,255), -1)
+img = cv2.circle(img, (x_center,x_center), 2, (0,0,0), -1, lineType=cv2.LINE_AA)
 
 cv2.imshow('modelTrain', img)
 cv2.waitKey(0)
